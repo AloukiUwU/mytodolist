1a20589ee5d9b0a87cef1444b146d3bc66d4e0bd
Deveosys <contact@deveosys.com>
2024-11-28 10:06:41 +0100

    Review

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 class Task {
-    constructor(name){
+    constructor(name) {
+        // Attention, même si tu utilises un grand nombre de random, il y a des chances que deux tâches aient le même id
+        // Une solution serait d'avoir une variable (const id = 0;) au début de ton fichier, et d'incrémenter cette variable à chaque nouvelle tâche
+        // ici ça donnerait this.id = ++id;
         this.id = Math.floor(Math.random() * 1000);
         this.name = name;
+        // Soit plus précis sur le nom de la variable, par exemple isDone au lieu de state
         this.state = false;
     }
 }
 
 class ToDoList {
-
-    constructor(){
+    constructor() {
         this.tasks = [];
         this.doneTasks = [];
     }
 
-    update(){
+    update() {
         //Updates the frontend display of the tasks
+        // ATTENTION : utilise let pour déclarer tes variables, laisse tomber var
+        // Tu n'as pas besoin des variables doneCount et notDoneCount, tu peux utiliser this.tasks.length et this.doneTasks.length
         var doneCount = 0;
         var notDoneCount = 0;
         var taskRatio = document.getElementById("taskRatio");
@@ -23,146 +28,147 @@ class ToDoList {
         var doneTaskHeader = document.getElementById("doneTaskHeader");
         var doneTaskList = document.getElementById("doneTaskList");
         var emptyBtn = document.getElementById("emptyBtn");
-        
+
         taskList.innerHTML = "";
         doneTaskList.innerHTML = "";
 
-        if(this.tasks.length > 0){
+        if (this.tasks.length > 0) {
             //Afficher le header des taches non faites et leur liste
             taskHeader.innerText = "Taches a faire";
 
-            this.tasks.forEach(task => {
+            // Ce que tu fais ici est OK ça fonctionne, en revanche je t'encourage à créer tes éléments HTML et paramétrer les observers en js :
+            // const li = document.createElement('li');
+            // li.id = task.id;
+            // const input = document.createElement('input');
+            // input.type = "checkbox";
+            // input.id = `${task.id}_check`;
+            // input.addEventListener("change", () => checkChanged(task.id));
+            // li.appendChild(input);
+            // etc...
+            // C'est plus lisible et ça évite les 'erreurs' de syntaxe
+            this.tasks.forEach((task) => {
                 notDoneCount++;
                 taskList.innerHTML += `<li id="${task.id}"><input type="checkbox" id="${task.id}_check" onchange=checkChanged(${task.id})>${task.name} <button onclick=removeTask(${task.id})>Supprimer</button>
-                </li>`;
+				</li>`;
             });
-        }
-        else {
+        } else {
             taskHeader.innerText = "";
         }
 
-        if(this.doneTasks.length > 0){
+        if (this.doneTasks.length > 0) {
             //Afficher le header des taches faites et leur liste
             doneTaskHeader.innerText = "Taches faites";
 
-            this.doneTasks.forEach(task => {
+            // Même remarque que pour les tâches non faites
+            this.doneTasks.forEach((task) => {
                 doneCount++;
                 doneTaskList.innerHTML += `<li id="${task.id}"><input type="checkbox" id="${task.id}_check" onchange=checkChanged(${task.id}) checked/>${task.name} <button onclick=removeTask(${task.id})>Supprimer</button>
-                </li>`;
+				</li>`;
             });
-        }
-        else {
+        } else {
             doneTaskHeader.innerText = "";
         }
 
-        if(doneCount > 0 || notDoneCount > 0){
-            taskRatio.innerText = `Total Ratio : ${doneCount}/${doneCount+notDoneCount}`
+        if (doneCount > 0 || notDoneCount > 0) {
+            taskRatio.innerText = `Total Ratio : ${doneCount}/${doneCount + notDoneCount}`;
             emptyBtn.disabled = false;
-        }
-        else {
+        } else {
             taskRatio.innerText = "";
             emptyBtn.disabled = true;
         }
     }
 
-    empty(){
+    empty() {
         this.tasks = [];
         this.doneTasks = [];
         this.update();
     }
 
-    remove(task){
+    remove(task) {
         //Removes a task from the list.
         var id;
-        if(task.state === false){
+        if (task.state === false) {
             id = this.tasks.indexOf(task);
             this.tasks.splice(id, 1);
-        } 
-        else {
+        } else {
             id = this.doneTasks.indexOf(task);
             this.doneTasks.splice(id, 1);
         }
         this.update();
     }
 
-    add(name){
+    add(name) {
         //Checks if taks with same name exists
         var exists = false;
-        this.tasks.forEach(task => {
-            if(task.name === name){
+        this.tasks.forEach((task) => {
+            if (task.name === name) {
                 exists = true;
             }
         });
-        this.doneTasks.forEach(task => {
-            if(task.name === name){
+        this.doneTasks.forEach((task) => {
+            if (task.name === name) {
                 exists = true;
             }
         });
 
-        if(exists === false){
+        if (exists === false) {
             //Add the entered task to the list
             this.tasks.push(new Task(name));
             document.getElementById("taskTitle").value = "";
             this.update();
-        }
-        else {
-            if (confirm("Une tache avec le meme nom existe deja. Souhaitez-vous quand meme l'ajouter ?")) 
-            {
+        } else {
+            if (confirm("Une tache avec le meme nom existe deja. Souhaitez-vous quand meme l'ajouter ?")) {
                 this.tasks.push(new Task(name));
                 document.getElementById("taskTitle").value = "";
                 this.update();
-            } 
+            }
         }
     }
 
-    addByObj(task){
-        if(task.state === true){
+    addByObj(task) {
+        if (task.state === true) {
             //Add to doneTasks
             this.doneTasks.push(task);
-        }
-        else {
+        } else {
             //Add to tasks
             this.tasks.push(task);
         }
     }
 }
 
-function removeTask(id){
+function removeTask(id) {
     var task = new Task();
-    toDoList.tasks.forEach(elmt => {
-        if(elmt.id === id) task = elmt;
+    toDoList.tasks.forEach((elmt) => {
+        if (elmt.id === id) task = elmt;
     });
 
-    if(task.name === undefined){
-        toDoList.doneTasks.forEach(elmt => {
-            if(elmt.id === id) task = elmt;
+    if (task.name === undefined) {
+        toDoList.doneTasks.forEach((elmt) => {
+            if (elmt.id === id) task = elmt;
         });
     }
 
     toDoList.remove(task);
 }
 
-function emptyTasks(){
+function emptyTasks() {
     toDoList.empty();
 }
 
-function checkChanged(id){
+function checkChanged(id) {
     //1. Find task object
     var task = new Task();
-    toDoList.tasks.forEach(elmt => {
-        if(elmt.id === id) task = elmt;
+    toDoList.tasks.forEach((elmt) => {
+        if (elmt.id === id) task = elmt;
     });
-    
-    if(task.name === undefined){
-        toDoList.doneTasks.forEach(elmt => {
-            if(elmt.id === id) task = elmt;
+
+    if (task.name === undefined) {
+        toDoList.doneTasks.forEach((elmt) => {
+            if (elmt.id === id) task = elmt;
         });
     }
 
-
-
-    if(document.getElementById(id+"_check").checked)
-    {
+    if (document.getElementById(id + "_check").checked) {
         //Has been checked
 
         //2. Remove from "tasks"
@@ -173,11 +179,9 @@ function checkChanged(id){
 
         //4. Add to "doneTasks"
         toDoList.addByObj(task);
-    }
-    else
-    {
+    } else {
         //Has been unchecked
-        
+
         //2. Remove from "doneTasks"
         toDoList.remove(task);
 
@@ -190,11 +194,10 @@ function checkChanged(id){
     toDoList.update();
 }
 
-var toDoList= new ToDoList();
-document.getElementById("addTaskForm")
-.addEventListener("submit", function (event) {
- event.preventDefault(); // Previent la soumission 'normale' du formulaire
-const name = document.getElementById("taskTitle").value;
-toDoList.add(name);
+var toDoList = new ToDoList();
+document.getElementById("addTaskForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Previent la soumission 'normale' du formulaire
+    const name = document.getElementById("taskTitle").value;
+    toDoList.add(name);
 });
 toDoList.update();
